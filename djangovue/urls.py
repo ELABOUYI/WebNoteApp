6bce8675fa203724beb86e59f42d9d0f52a14514
elabouyi <y.elabouyi1u@gmail.com>
2021-12-01 11:47:36 +0100

    Create the Note app

--- djangovue/urls.py
+++ djangovue/urls.py
@@ -19,11 +19,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 vue_urls = [
-  path('', lambda request: HttpResponse(render(request, 'vue_index.html'))),
-  path('another-path/', lambda request: HttpResponse(render(request, 'vue_index.html'))),
+    path("", lambda request: HttpResponse(render(request, "vue_index.html"))),
+    path(
+        "another-path/", lambda request: HttpResponse(render(request, "vue_index.html"))
+    ),
 ]
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include(vue_urls)),
+    path("admin/", admin.site.urls),
+    path("", include(vue_urls)),
 ]
